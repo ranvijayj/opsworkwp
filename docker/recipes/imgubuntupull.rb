5872fb0e08a8d9e1ad55b62ce93e22ae18060ec8
ranvijayj <ranvijayj@intelligrape.com>
2015-07-23 15:04:21 +0530

    prashant 1

--- docker/recipes/imgubuntupull.rb
+++ docker/recipes/imgubuntupull.rb
@@ -1,9 +1,15 @@
-docker_service 'default' do
-  action [:create, :start]
-end
-
-docker_image 'ubuntu' do
-  action :pull
+#docker_service 'default' do
+#  action [:create, :start]
+#end
+#
+#docker_image 'ubuntu' do
+#  action :pull
+#end
+bash "image pll" do
+    user "root"
+    code <<-EOH
+        docker pull ubuntu
+    EOH
 end
 
 #docker_container 'test server' do
